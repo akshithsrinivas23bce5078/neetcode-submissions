97ba5c79c46547f868919f45675792d331132463
thisisAkshithSrinivas <154672757+akshithsrinivas23bce5078@users.noreply.github.com>
2026-08-01 17:53:55 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer>list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(i != j && nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
